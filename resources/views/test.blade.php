54ddf47d5d195a969d91b276cfca4fac420e43ce
ADRIAN <adriandeligero.1030@gmail.com>
2025-03-05 16:52:11 +0800

    feat: added login form

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -7,7 +7,31 @@
     @vite(['resources/js/app.js'])
 </head>
 <body>
-    <button class="btn btn-danger">Bootstrap Button</button>
+      <div class="container mt-5 vh-100 ">
+        <div class="row justify-content-center " >
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="text-center">Login</h3>
+                    </div>
+                    <div class="card-body">
+                        <form method="POST" >
+                            @csrf
+                            <div class="form-group">
+                                <label for="email">Email address</label>
+                                <input type="email" class="form-control" id="email" name="email" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" class="form-control" id="password" name="password" required>
+                            </div>
+                            <button type="submit" class="btn btn-primary btn-block">Login</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </body>
 </html>
