7951b322abe18039f54d6153b988a321381d66a2
ADRIAN <adriandeligero.1030@gmail.com>
2025-03-05 09:55:02 +0800

    styles: configured and added bootstrap

---
+++ resources/views/test.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap Test</title>
+    @vite(['resources/js/app.js'])
+</head>
+<body>
+    <button class="btn btn-danger">Bootstrap Button</button>
+
+</body>
+</html>
