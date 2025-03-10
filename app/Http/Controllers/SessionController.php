28e1379b31e1f6ddc6deb8ccd90642d01b290c5b
ADRIAN <adriandeligero.1030@gmail.com>
2025-03-10 19:52:39 +0800

    fixed: login routes

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -7,6 +7,24 @@ use Illuminate\Http\Request;
 
 class SessionController extends Controller
 {
+    public function store()
+    {
+        $validated = request()->validate([
+             'email' => 'required|email',
+             'password' => 'required'
+         ]);
+        if (Auth::attempt($validated)) {
+            session()->regenerate();
+            return redirect('/hero');
+        }
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.'
+        ]);
+    }
+    public function logUser()
+    {
+        return view('Auth.login');
+    }
     public function destroy()
     {
         Auth::logout();
